8b9bbd8496dbc7d8e294aa96041d3a8e71939938
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-10-23 20:00:41 +0500

    Client File Done

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.listen(port, () => {
   console.log(`Server is listeing on port ${port}`);
 });
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello Express");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register_form", { title: "Registration Form" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login_form", { title: "Login Form" });
+});
+
+app.get("/forget", (req, res) => {
+  res.render("reset_form", { title: "Forget Form" });
 });
