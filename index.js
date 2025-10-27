63f3389b6724d0448971f3c7e0994538b777ec71
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-10-27 19:54:55 +0500

    DATABASE CONNECTION

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 const app = express();
 const port = 3000;
+import mongoose from "mongoose";
 
-app.listen(port, () => {
-  console.log(`Server is listeing on port ${port}`);
+// database connection
+mongoose.connect("mongodb://localhost:27017/users_data").then(() => {
+  console.log("Database Connected");
 });
 
 app.set("view engine", "ejs");
@@ -22,4 +24,8 @@ app.get("/login", (req, res) => {
 
 app.get("/forget", (req, res) => {
   res.render("reset_form", { title: "Forget Form" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listeing on port ${port}`);
 });
