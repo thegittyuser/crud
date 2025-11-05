0df6859f65f1733ad27201e24d4a8fad011e3fae
thegittyuser <webdevsserv@gmail.com>
2025-11-05 20:00:02 +0500

    MVC

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
 import userRouter from "./routes/usersdata.routes.js"
-import dbConnect from "./config/dbconnection.js";
+import mongodb from "./config/dbconnection.js";
 import express from "express";
 const app = express();
 const port = 3000;
 
-
-
 // database connection callback
-dbConnect();
-
+mongodb()
 app.set("view engine", "ejs");
 // middleware
 app.use(express.urlencoded({ extended: false }));
